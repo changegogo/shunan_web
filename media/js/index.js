d25dd04ae3239a9381a8f0dd7d63381df8779fe5
dailliwang <758017188@qq.com>
2017-12-05 16:26:57 +0800

    蜀南web

---
+++ media/js/index.js
@@ -0,0 +1,11 @@
+var Index = function () {
+    return {
+        init: function () {
+            console.log("init");
+        },
+        initTable: function(){
+        	
+        }
+    };
+
+}();
